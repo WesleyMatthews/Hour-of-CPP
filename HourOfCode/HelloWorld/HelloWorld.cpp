ec70821c4482bc51ad34da9e63bc5417cc507ce3
Wesley Matthews <43826609+GamifyEducation@users.noreply.github.com>
2020-09-08 19:37:23 -0500

    Initial push

---
+++ HourOfCode/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,12 @@
+// Tells the computer that we need the "iostream" library, which deals with input and output
+#include <iostream>
+
+// Creates the main function to be run, and returns an integer
+int main()
+{
+    // Prints "Hello World!" and a new line to the console
+    std::cout << "Hello World!\n";
+
+    // Exits the function with a value of 0
+    return 0;
+}
